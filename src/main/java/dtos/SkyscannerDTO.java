e3a55116fc7414f92e13e45fba08d67f531aeeb3
Bringordie <48799015+Bringordie@users.noreply.github.com>
2020-04-14 13:36:53 +0200

    added dto package

---
+++ src/main/java/dtos/SkyscannerDTO.java
@@ -0,0 +1,18 @@
+package dtos;
+
+import entities.Skyscanner;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SkyscannerDTO {
+    
+    
+    public SkyscannerDTO(Skyscanner skyscanner) {
+        
+    }
+
+    public SkyscannerDTO() {
+    }
+
+    
+}
