e31ba06cf2dda28786a758133fd26fac03878784
Frederiket1912 <44972163+Frederiket1912@users.noreply.github.com>
2020-04-14 15:31:01 +0200

    Added HTTPUtils, some dtos and made a mess of threads in endpoint.

--- src/main/java/rest/DemoResource.java
+++ src/main/java/rest/DemoResource.java
@@ -1,8 +1,22 @@
 package rest;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import dtos.ChuckDTO;
+import dtos.DadDTO;
+import dtos.JSONPlaceholderDTO;
+import dtos.SkyscannerDTO;
 import entities.User;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.annotation.security.RolesAllowed;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -14,6 +28,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.SecurityContext;
 import utils.EMF_Creator;
+import utils.HttpUtils;
 
 /**
  * @author lam@cphbusiness.dk
@@ -67,4 +82,62 @@ public class DemoResource {
         String thisuser = securityContext.getUserPrincipal().getName();
         return "{\"msg\": \"Hello to (admin) User: " + thisuser + "\"}";
     }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @RolesAllowed({"admin", "user"})
+    public String getJokes() throws IOException, InterruptedException {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+//        List<String> urls = new ArrayList<>();
+//        urls.add("https://api.chucknorris.io/jokes/random");
+//        urls.add("https://icanhazdadjoke.com");
+//        urls.add("https://jsonplaceholder.typicode.com/todos/1");
+//        urls.add("https://api.chucknorris.io/jokes/random");
+//        urls.add("https://api.chucknorris.io/jokes/random");
+
+        ChuckDTO chuckDTO;
+        DadDTO dadDTO = null;
+        JSONPlaceholderDTO jpDTO = null;
+        SkyscannerDTO skyscannerDTO = null;
+
+        ExecutorService workingJack = Executors.newFixedThreadPool(5);
+
+         Runnable task  = new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                    String chuck = HttpUtils.fetchData("https://api.chucknorris.io/jokes/random");
+                    chuckDTO = gson.fromJson(chuck, ChuckDTO.class);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                }
+            };
+            workingJack.submit(task);   
+
+//        Runnable task2 = () -> {
+//            String dad = HttpUtils.fetchData("https://icanhazdadjoke.com");
+//            dadDTO = gson.fromJson(dad, DadDTO.class);
+//        };
+//        workingJack.submit(task2);
+//
+//        Runnable task3 = () -> {
+//            String ph = HttpUtils.fetchData("https://jsonplaceholder.typicode.com/todos/1");
+//            jpDTO = gson.fromJson(ph, JSONPlaceholderDTO.class);
+//        };
+//        workingJack.submit(task3);
+
+        workingJack.shutdown();
+        workingJack.awaitTermination(15, TimeUnit.SECONDS);
+
+//        String chuck = HttpUtils.fetchData("https://api.chucknorris.io/jokes/random");
+//        ChuckDTO chuckDTO = gson.fromJson(chuck, ChuckDTO.class);
+//        String dad = HttpUtils.fetchData("https://icanhazdadjoke.com");
+//        DadDTO dadDTO = gson.fromJson(dad, DadDTO.class);
+        //CombinedDTO combinedDTO = new CombinedDTO(dadDTO, chuckDTO);
+        //This is what your endpoint should return       
+        //String combinedJSON = gson.toJson(combinedDTO);
+        //return combinedJSON;
+    }
+
 }
